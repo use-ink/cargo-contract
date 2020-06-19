3b0368a0d2334a6cb06bb5731fd0b80d98a0b9e2
Andrew Jones <ascjones@gmail.com>
2020-06-19 09:31:47 +0100

    ink! v2 compatibility

--- src/workspace/abi.rs
+++ src/workspace/abi.rs
@@ -32,14 +32,14 @@ name = "abi-gen"
 path = "main.rs"
 
 [dependencies]
-contract = { path = "../..", package = "{{name}}" }
+contract = { path = "../.." }
 serde = "1.0"
 serde_json = "1.0"
 "#;
 
 const MAIN_RS: &str = r#"
 fn main() -> Result<(), std::io::Error> {
-    let abi = <contract::{{camel_name}} as ink_lang::GenerateAbi>::generate_abi();
+    let abi = <::contract::{{camel_name}} as ::ink_lang::GenerateAbi>::generate_abi();
     let contents = serde_json::to_string_pretty(&abi)?;
     std::fs::create_dir("target").ok();
     std::fs::write("target/metadata.json", contents)?;
@@ -54,22 +54,37 @@ pub(super) fn generate_package<P: AsRef<Path>>(
 ) -> Result<()> {
     let dir = dir.as_ref();
     log::debug!("Generating abi package for {} in {}", name, dir.display());
-    // replace template placeholders
-    let cargo_toml = CARGO_TOML.replace("{{name}}", name);
-    let main_rs = MAIN_RS
-        .replace("{{name}}", name)
-        // todo: [AJ] can the contract struct name be accurately inferred and passed in?
-        .replace("{{camel_name}}", &name.to_string().to_camel_case());
 
-    // add ink_lang dependency
-    let mut cargo_toml: value::Table = toml::from_str(&cargo_toml)?;
+    let mut cargo_toml: value::Table = toml::from_str(CARGO_TOML)?;
     let deps = cargo_toml
         .get_mut("dependencies")
         .expect("[dependencies] section specified in the template")
         .as_table_mut()
         .expect("[dependencies] is a table specified in the template");
+
+    // initialize contract dependency
+    let contract = deps
+        .get_mut("contract")
+        .expect("contract dependency specified in the template")
+        .as_table_mut()
+        .expect("contract dependency is a table specified in the template");
+    contract.insert("package".into(), name.into());
+
+    // ink v2 compat
+    if ink_lang.get("version") == Some(&value::Value::String("2".into())) {
+        contract.insert("default-features".into(), false.into());
+        contract.insert("features".into(), vec!["ink-generate-abi"].into());
+    }
+
+    // add ink_lang dependency
     deps.insert("ink_lang".into(), ink_lang.into());
     let cargo_toml = toml::to_string(&cargo_toml)?;
+
+    // replace main.rs template placeholders
+    let main_rs = MAIN_RS
+        .replace("{{name}}", name)
+        // todo: [AJ] can the contract struct name be accurately inferred and passed in?
+        .replace("{{camel_name}}", &name.to_string().to_camel_case());
 
     fs::write(dir.join("Cargo.toml"), cargo_toml)?;
     fs::write(dir.join("main.rs"), main_rs)?;
