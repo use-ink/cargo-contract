972287a6f7fafe3d3301f485627b9ae799b7224d
Andrew Jones <ascjones@gmail.com>
2020-07-08 09:46:17 +0100

    Flatten metadata struct

--- src/cmd/metadata/mod.rs
+++ src/cmd/metadata/mod.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with ink!.  If not, see <http://www.gnu.org/licenses/>.
 
-mod project;
+mod contract;
 
 use crate::{
     util,
