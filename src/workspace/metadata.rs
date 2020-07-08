c29225b7fa1019145f892f4c3e8030462c1edec6
Andrew Jones <ascjones@gmail.com>
2020-07-08 09:01:31 +0100

    Revert "Generate main.rs with quote for passing extension metadata"
    This reverts commit 0929ce8a

--- src/workspace/metadata.rs
+++ src/workspace/metadata.rs
@@ -29,7 +29,7 @@ pub(super) fn generate_package<P: AsRef<Path>>(
     target_dir: P,
     contract_package_name: &str,
     ink_lang_dependency: value::Table,
-    ink_metadata_dependency: value::Table,
+    mut ink_abi_dependency: value::Table,
 ) -> Result<()> {
     let dir = target_dir.as_ref();
     log::debug!(
@@ -38,31 +38,17 @@ pub(super) fn generate_package<P: AsRef<Path>>(
         dir.display()
     );
 
-    let main_rs = generate_main();
-    let cargo_toml = generate_cargo_toml(contract_package_name, ink_lang_dependency, ink_metadata_dependency)?;
+    let cargo_toml = include_str!("../../templates/tools/generate-metadata/_Cargo.toml");
+    let main_rs = include_str!("../../templates/tools/generate-metadata/main.rs");
 
-    fs::write(dir.join("Cargo.toml"), cargo_toml)?;
-    fs::write(dir.join("main.rs"), main_rs)?;
-    Ok(())
-}
-
-/// Generates the `Cargo.toml` file for the `metadata-gen` package
-fn generate_cargo_toml(
-    contract_package_name: &str,
-    ink_lang_dependency: value::Table,
-    mut ink_metadata_dependency: value::Table
-) -> Result<String> {
-    let template = include_str!("../../templates/tools/generate-metadata/_Cargo.toml");
-    let mut cargo_toml: value::Table = toml::from_str(template)?;
-
-    // get a mutable reference to the dependencies section
+    let mut cargo_toml: value::Table = toml::from_str(cargo_toml)?;
     let deps = cargo_toml
         .get_mut("dependencies")
         .expect("[dependencies] section specified in the template")
         .as_table_mut()
         .expect("[dependencies] is a table specified in the template");
 
-    // initialize the contract dependency
+    // initialize contract dependency
     let contract = deps
         .get_mut("contract")
         .expect("contract dependency specified in the template")
@@ -70,82 +56,18 @@ fn generate_cargo_toml(
         .expect("contract dependency is a table specified in the template");
     contract.insert("package".into(), contract_package_name.into());
 
-    // make ink_metadata dependency use default features
-    ink_metadata_dependency.remove("default-features");
-    ink_metadata_dependency.remove("features");
-    ink_metadata_dependency.remove("optional");
+    // make ink_abi dependency use default features
+    ink_abi_dependency.remove("default-features");
+    ink_abi_dependency.remove("features");
+    ink_abi_dependency.remove("optional");
 
     // add ink dependencies copied from contract manifest
     deps.insert("ink_lang".into(), ink_lang_dependency.into());
     deps.insert("ink_metadata".into(), ink_metadata_dependency.into());
 
     let cargo_toml = toml::to_string(&cargo_toml)?;
-    Ok(cargo_toml)
-}
 
-/// Generate a `main.rs` to invoke `__ink_generate_metadata`
-fn generate_main() -> String {
-    quote::quote! (
-        extern crate contract;
-
-        extern "Rust" {
-            fn __ink_generate_metadata(
-                extension: ::ink_metadata::InkProjectExtension
-            ) -> ::ink_metadata::InkProject;
-        }
-
-        use ::ink_metadata::{
-            Compiler,
-            CompilerInfo,
-            InkProjectContract,
-            InkProjectExtension,
-            InkProjectSource,
-            InkProjectUser,
-            Language,
-            SourceCompiler,
-            SourceLanguage,
-            Version,
-            Url,
-        };
-
-        fn main() -> Result<(), std::io::Error> {
-            // todo: pass in the following as args to generate_main()
-            let wasm_hash = [0u8; 32];
-            let ink_version = Version::new(2, 1, 0);
-            let rustc_version = Version::new(1, 46, 0);
-
-            let extension = {
-                let language = SourceLanguage::new(Language::Ink, ink_version.clone());
-                let compiler = SourceCompiler::new(
-                    CompilerInfo::new(Compiler::Ink, ink_version),
-                    CompilerInfo::new(Compiler::RustC, rustc_version),
-                );
-                let source = InkProjectSource::new(
-                    wasm_hash,
-                    language,
-                    compiler,
-                );
-
-                let contract =
-                    InkProjectContract::build()
-                        .name("testing")
-                        .version(Version::new(0, 1, 0))
-                        .authors(vec!["author@example.com"])
-                        .documentation(Url::parse("http://example.com").unwrap())
-                        .done();
-
-                // todo: pass in user args
-                let user: Option<InkProjectUser> = None;
-
-                InkProjectExtension::new(source, contract, user)
-            };
-
-            let ink_project = unsafe { __ink_generate_metadata(extension) };
-
-            let contents = serde_json::to_string_pretty(&ink_project)?;
-            std::fs::create_dir("target").ok();
-            std::fs::write("target/metadata.json", contents)?;
-            Ok(())
-        }
-    ).to_string()
+    fs::write(dir.join("Cargo.toml"), cargo_toml)?;
+    fs::write(dir.join("main.rs"), main_rs)?;
+    Ok(())
 }
