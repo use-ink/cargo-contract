e9f6836f518d056ca73cb3ac5c98e9f46b94f77b
Andrew Jones <ascjones@gmail.com>
2020-07-09 12:21:47 +0100

    Update file headers replacing ink!

--- src/workspace/profile.rs
+++ src/workspace/profile.rs
@@ -1,18 +1,18 @@
 // Copyright 2018-2020 Parity Technologies (UK) Ltd.
 // This file is part of cargo-contract.
 //
-// ink! is free software: you can redistribute it and/or modify
+// cargo-contract is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// ink! is distributed in the hope that it will be useful,
+// cargo-contract is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with ink!.  If not, see <http://www.gnu.org/licenses/>.
+// along with cargo-contract.  If not, see <http://www.gnu.org/licenses/>.
 
 use toml::value;
 
