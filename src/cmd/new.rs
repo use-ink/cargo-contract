caf77bb49d4f8d63cbf21873c4f22add5d5992c5
Andrew Jones <ascjones@gmail.com>
2020-10-05 15:43:46 +0100

    Fix deprecation warnings

--- src/cmd/new.rs
+++ src/cmd/new.rs
@@ -57,7 +57,7 @@ where
         let contents = contents.replace("{{name}}", name);
         let contents = contents.replace("{{camel_name}}", &name.to_camel_case());
 
-        let outpath = out_dir.join(file.sanitized_name());
+        let outpath = out_dir.join(file.name());
 
         if (&*file.name()).ends_with('/') {
             fs::create_dir_all(&outpath)?;
@@ -75,7 +75,7 @@ where
                     if e.kind() == std::io::ErrorKind::AlreadyExists {
                         anyhow::anyhow!(
                             "New contract file {} already exists",
-                            file.sanitized_name().display()
+                            file.name().display()
                         )
                     } else {
                         anyhow::anyhow!(e)
