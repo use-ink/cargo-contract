70f9f5c8ba3290d43602b9c281f55479634cb091
Andrew Jones <ascjones@gmail.com>
2020-10-19 09:30:42 +0100

    Fix license headers

--- src/cmd/extrinsics/transcode/ronext/parse.rs
+++ src/cmd/extrinsics/transcode/ronext/parse.rs
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with cargo-contract.  If not, see <http://www.gnu.org/license
+// along with cargo-contract.  If not, see <http://www.gnu.org/licenses/>.
 
 use nom::{
     branch::alt,
