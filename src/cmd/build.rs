f8246d39905d070b2ff846560304ae64f11a3a41
Michael Mueller <mich@elmueller.net>
2020-11-06 15:41:44 +0100

    Apply cargo fmt

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -339,8 +339,15 @@ mod tests {
             cmd::new::execute("new_project", Some(path)).expect("new project creation failed");
             let manifest_path =
                 ManifestPath::new(&path.join("new_project").join("Cargo.toml")).unwrap();
-            super::execute(&manifest_path, None, true, false, false, UnstableFlags::default())
-                .expect("build failed");
+            super::execute(
+                &manifest_path,
+                None,
+                true,
+                false,
+                false,
+                UnstableFlags::default(),
+            )
+            .expect("build failed");
             Ok(())
         })
     }
