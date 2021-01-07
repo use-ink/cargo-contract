05a2505cd31545187b169a33a9827b7a91cfa03c
honeywest <honeywest@foxmail.com>
2021-01-07 09:25:34 +0800

    feat build execute code

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -300,8 +300,8 @@ fn execute(
     build_artifact: BuildArtifacts,
     unstable_flags: UnstableFlags,
 ) -> Result<BuildResult> {
-    let crate_metadata = CrateMetadata::collect(manifest_path)?;
     if build_artifact == BuildArtifacts::CodeOnly || build_artifact == BuildArtifacts::CheckOnly {
+        let crate_metadata = CrateMetadata::collect(manifest_path)?;
         let (maybe_dest_wasm, maybe_optimization_result) = execute_with_crate_metadata(
             &crate_metadata,
             verbosity,
