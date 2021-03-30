812ada4d56930b868a3ee8a9f9c4ca7c56ae7d91
Andrew Jones <ascjones@gmail.com>
2021-03-30 11:14:52 +0100

    Check wasm optimized output file exists

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -282,6 +282,10 @@ fn optimize_wasm(
         optimization_passes,
     )?;
 
+    if !dest_optimized.exists() {
+        return Err(anyhow::anyhow!("Optimization failed, optimized wasm output file not found."))
+    }
+
     let original_size = metadata(&crate_metadata.dest_wasm)?.len() as f64 / 1000.0;
     let optimized_size = metadata(&dest_optimized)?.len() as f64 / 1000.0;
 
