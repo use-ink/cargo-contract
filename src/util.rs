307de23e463aa555cb2bf6a3a75632e7c4e0770d
Michael Müller <michi@parity.io>
2021-04-20 15:19:53 +0200

    Fix flaky CI test (#263)
    * Add fix
    
    * Revert "Add fix"
    
    This reverts commit 5c81fee558d59379683219e78c1e77870dd9232b.
    
    * Generate unique contract names for tests
    
    * Fix test
    
    * Implement comments
    
    * Implement comments

--- src/util.rs
+++ src/util.rs
@@ -104,8 +104,12 @@ macro_rules! maybe_println {
 
 #[cfg(test)]
 pub mod tests {
+    use crate::ManifestPath;
     use std::path::Path;
+    use std::sync::atomic::{AtomicU32, Ordering};
 
+    /// Creates a temporary directory and passes the `tmp_dir` path to `f`.
+    /// Panics if `f` returns an `Err`.
     pub fn with_tmp_dir<F>(f: F)
     where
         F: FnOnce(&Path) -> anyhow::Result<()>,
@@ -117,5 +121,43 @@ pub mod tests {
 
         // catch test panics in order to clean up temp dir which will be very large
         f(tmp_dir.path()).expect("Error executing test with tmp dir")
+    }
+
+    /// Global counter to generate unique contract names in `with_new_contract_project`.
+    ///
+    /// We typically use `with_tmp_dir` to generate temporary folders to build contracts
+    /// in. But for caching purposes our CI uses `CARGO_TARGET_DIR` to overwrite the
+    /// target directory of any contract build -- it is set to a fixed cache directory
+    /// instead.
+    /// This poses a problem since we still want to ensure that each test builds to its
+    /// own, unique target directory -- without interfering with the target directory of
+    /// other tests. In the past this has been a problem when a test tried to create a
+    /// contract with the same contract name as another test -- both were then build
+    /// into the same target directory, sometimes causing test failures for strange reasons.
+    ///
+    /// The fix we decided on is to append a unique number to each contract name which
+    /// is created. This `COUNTER` provides a global counter which is accessed by each test
+    /// (in each thread) to get the current `COUNTER` number and increase it afterwards.
+    ///
+    /// We decided to go for this counter instead of hashing (with e.g. the temp dir) to
+    /// prevent an infinite number of contract artifacts being created in the cache directory.
+    static COUNTER: AtomicU32 = AtomicU32::new(0);
+
+    /// Creates a new contract into a temporary directory. The contract's
+    /// `ManifestPath` is passed into `f`.
+    pub fn with_new_contract_project<F>(f: F)
+    where
+        F: FnOnce(ManifestPath) -> anyhow::Result<()>,
+    {
+        with_tmp_dir(|tmp_dir| {
+            let unique_name = format!("new_project_{}", COUNTER.fetch_add(1, Ordering::SeqCst));
+
+            crate::cmd::new::execute(&unique_name, Some(tmp_dir))
+                .expect("new project creation failed");
+            let working_dir = tmp_dir.join(unique_name);
+            let manifest_path = ManifestPath::new(working_dir.join("Cargo.toml"))?;
+
+            f(manifest_path)
+        })
     }
 }
