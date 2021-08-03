d2f4c863f304fc255c41d2c430429b546a93500e
Michael Mueller <mich@elmueller.net>
2021-08-03 08:56:21 +0200

    Debug path

--- src/workspace/manifest.rs
+++ src/workspace/manifest.rs
@@ -369,7 +369,9 @@ impl Manifest {
             let path_str = existing_path
                 .as_str()
                 .ok_or_else(|| anyhow::anyhow!("{} should be a string", value_id))?;
-            let path = PathBuf::from(path_str);
+            eprinltn!("path: {:?}", OsString::from(path_str));
+            eprinltn!("path: {:?}", OsString::from(path_str).as_os_str());
+            let path = PathBuf::from(OsString::from(path_str));
             if path.is_relative() {
                 eprintln!("path is relative");
                 let lib_abs = abs_dir.join(path);
