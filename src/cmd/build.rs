0bd2629dd2be4f407d7f1dc36ac03d2970767554
Alexander Theißen <alex.theissen@me.com>
2021-08-12 13:49:39 +0200

    Do not strip out panic messages in debug builds

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -210,13 +210,14 @@ fn exec_cargo_for_wasm_target(
         let mut args = vec![
             "--target=wasm32-unknown-unknown",
             "-Zbuild-std",
-            "-Zbuild-std-features=panic_immediate_abort",
             "--no-default-features",
             "--release",
             &target_dir,
         ];
         if build_mode == BuildMode::Debug {
             args.push("--features=ink_env/ink-debug");
+        } else {
+            args.push("-Zbuild-std-features=panic_immediate_abort");
         }
         util::invoke_cargo(command, &args, manifest_path.directory(), verbosity)?;
 
