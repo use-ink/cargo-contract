3c2f293b58fbe239372d2c325270b6365cd05ae7
Andrew Jones <ascjones@gmail.com>
2021-10-19 20:44:54 +0100

    Update to latest subxt

--- src/cmd/extrinsics/runtime_api/mod.rs
+++ src/cmd/extrinsics/runtime_api/mod.rs
@@ -14,49 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with cargo-contract.  If not, see <http://www.gnu.org/licenses/>.
 
-use sp_runtime::traits::BlakeTwo256;
-use subxt::{subxt, Runtime, StorageEntry};
-
 // todo: [AJ] use current file location as relative path for this file?
 // todo: [AJ] regenerate metadata for contract template node (it will likely be smaller)
-#[subxt(runtime_metadata_path = "src/cmd/extrinsics/runtime_api/contracts_runtime.scale")]
-pub mod api {
-    #[subxt(substitute_type = "sp_core::crypto::AccountId32")]
-    use sp_core::crypto::AccountId32;
-    #[subxt(substitute_type = "primitive_types::H256")]
-    use sp_core::H256;
-    #[subxt(substitute_type = "sp_runtime::multiaddress::MultiAddress")]
-    use sp_runtime::MultiAddress;
-
-    #[subxt(substitute_type = "sp_arithmetic::per_things::Perbill")]
-    use sp_arithmetic::per_things::Perbill;
-    #[subxt(substitute_type = "sp_arithmetic::per_things::Perquintill")]
-    use sp_arithmetic::per_things::Perquintill;
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct ContractsRuntime;
-
-impl Runtime for ContractsRuntime {
-    type Index = u32;
-    type BlockNumber = u32;
-    type Hash = sp_core::H256;
-    type Hashing = BlakeTwo256;
-    type AccountId = sp_runtime::AccountId32;
-    type Address = sp_runtime::MultiAddress<Self::AccountId, u32>;
-    type Header = sp_runtime::generic::Header<Self::BlockNumber, BlakeTwo256>;
-    type Extra = subxt::extrinsic::DefaultExtra<Self>;
-    type Signature = sp_runtime::MultiSignature;
-    type Extrinsic = sp_runtime::OpaqueExtrinsic;
-    type AccountData = api::system::storage::Account;
-}
-
-impl subxt::AccountData<ContractsRuntime> for api::system::storage::Account {
-    fn new(account_id: <ContractsRuntime as Runtime>::AccountId) -> Self {
-        Self(account_id.into())
-    }
-
-    fn nonce(result: &<Self as StorageEntry>::Value) -> <ContractsRuntime as Runtime>::Index {
-        result.nonce
-    }
-}
+#[subxt::subxt(runtime_metadata_path = "src/cmd/extrinsics/runtime_api/contracts_runtime.scale")]
+pub mod api { }
