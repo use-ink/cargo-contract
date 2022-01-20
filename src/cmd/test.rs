cc9a76ca31a06394bb2eb148fb201dc4683f8201
Michael Mueller <mich@elmueller.net>
2022-01-20 13:33:47 +0100

    Experiment with env variables

--- src/cmd/test.rs
+++ src/cmd/test.rs
@@ -65,9 +65,6 @@ pub(crate) fn execute(manifest_path: &ManifestPath, verbosity: Verbosity) -> Res
         "Running tests".bright_green().bold()
     );
 
-    // We need to make sure that `RUSTFLAGS` are not set in the CI here.
-    // `cargo-contract` sets them when building contracts.
-    std::env::set_var("RUSTFLAGS", "");
     let stdout = util::invoke_cargo("test", &[""], manifest_path.directory(), verbosity)?;
 
     Ok(TestResult { stdout, verbosity })
