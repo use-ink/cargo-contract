07ecad712d1534b3503bdd0fb023c507c96e0061
Squirrel <gilescope@gmail.com>
2022-07-01 12:48:39 +0100

    simplify hex parsing (#628)

--- transcode/src/util.rs
+++ transcode/src/util.rs
@@ -16,9 +16,5 @@
 
 /// Decode hex string with or without 0x prefix
 pub fn decode_hex(input: &str) -> Result<Vec<u8>, hex::FromHexError> {
-    if input.starts_with("0x") {
-        hex::decode(input.trim_start_matches("0x"))
-    } else {
-        hex::decode(input)
-    }
+    hex::decode(input.trim_start_matches("0x"))
 }
