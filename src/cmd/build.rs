bbec6570e8083736e7f256cf42b75694f09b92d1
Hernando Castano <hernando@hcastano.com>
2022-08-05 12:57:58 -0400

    Remove old Wasm optimization functions

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -536,128 +536,6 @@ fn post_process_wasm(crate_metadata: &CrateMetadata) -> Result<()> {
     Ok(())
 }
 
-/// Attempts to perform optional Wasm optimization using `binaryen`.
-///
-/// The intention is to reduce the size of bloated Wasm binaries as a result of missing
-/// optimizations (or bugs?) between Rust and Wasm.
-fn optimize_wasm(
-    crate_metadata: &CrateMetadata,
-    optimization_passes: OptimizationPasses,
-    keep_debug_symbols: bool,
-) -> Result<OptimizationResult> {
-    let mut dest_optimized = crate_metadata.dest_wasm.clone();
-    dest_optimized.set_file_name(format!(
-        "{}-opt.wasm",
-        crate_metadata.contract_artifact_name
-    ));
-
-    // do_optimization(
-    //     crate_metadata.dest_wasm.as_os_str(),
-    //     dest_optimized.as_os_str(),
-    //     optimization_passes,
-    //     keep_debug_symbols,
-    // )?;
-
-    // NANDO: This should probably be part of the WasmOptHandler as well
-    // Maybe optimize can return an `OptimizationResult`
-
-    if !dest_optimized.exists() {
-        return Err(anyhow::anyhow!(
-            "Optimization failed, optimized wasm output file `{}` not found.",
-            dest_optimized.display()
-        ))
-    }
-
-    // TODO: Maybe get this from the handler instead of reading it from a file?
-    let original_size = metadata(&crate_metadata.dest_wasm)?.len() as f64 / 1000.0;
-    let optimized_size = metadata(&dest_optimized)?.len() as f64 / 1000.0;
-
-    // overwrite existing destination wasm file with the optimised version
-    std::fs::rename(&dest_optimized, &crate_metadata.dest_wasm)?;
-    Ok(OptimizationResult {
-        dest_wasm: crate_metadata.dest_wasm.clone(),
-        original_size,
-        optimized_size,
-    })
-}
-
-/// Optimizes the Wasm supplied as `crate_metadata.dest_wasm` using
-/// the `wasm-opt` binary.
-///
-/// The supplied `optimization_level` denotes the number of optimization passes,
-/// resulting in potentially a lot of time spent optimizing.
-///
-/// If successful, the optimized Wasm is written to `dest_optimized`.
-fn do_optimization(
-    dest_wasm: &OsStr,
-    dest_optimized: &OsStr,
-    optimization_level: OptimizationPasses,
-    keep_debug_symbols: bool,
-) -> Result<()> {
-    // check `wasm-opt` is installed
-    let which = which::which("wasm-opt");
-    if which.is_err() {
-        anyhow::bail!(
-            "wasm-opt not found! Make sure the binary is in your PATH environment.\n\n\
-            We use this tool to optimize the size of your contract's Wasm binary.\n\n\
-            wasm-opt is part of the binaryen package. You can find detailed\n\
-            installation instructions on https://github.com/WebAssembly/binaryen#tools.\n\n\
-            There are ready-to-install packages for many platforms:\n\
-            * Debian/Ubuntu: apt-get install binaryen\n\
-            * Homebrew: brew install binaryen\n\
-            * Arch Linux: pacman -S binaryen\n\
-            * Windows: binary releases at https://github.com/WebAssembly/binaryen/releases"
-                .to_string()
-                .bright_yellow()
-        );
-    }
-    let wasm_opt_path = which
-        .as_ref()
-        .expect("we just checked if `which` returned an err; qed")
-        .as_path();
-    log::info!("Path to wasm-opt executable: {}", wasm_opt_path.display());
-
-    check_wasm_opt_version_compatibility(wasm_opt_path)?;
-
-    log::info!(
-        "Optimization level passed to wasm-opt: {}",
-        optimization_level
-    );
-    let mut command = Command::new(wasm_opt_path);
-    command
-        .arg(dest_wasm)
-        .arg(format!("-O{}", optimization_level))
-        .arg("-o")
-        .arg(dest_optimized)
-        // the memory in our module is imported, `wasm-opt` needs to be told that
-        // the memory is initialized to zeroes, otherwise it won't run the
-        // memory-packing pre-pass.
-        .arg("--zero-filled-memory");
-    if keep_debug_symbols {
-        command.arg("-g");
-    }
-    log::info!("Invoking wasm-opt with {:?}", command);
-    let output = command.output().map_err(|err| {
-        anyhow::anyhow!(
-            "Executing {} failed with {:?}",
-            wasm_opt_path.display(),
-            err
-        )
-    })?;
-
-    if !output.status.success() {
-        let err = str::from_utf8(&output.stderr)
-            .expect("Cannot convert stderr output of wasm-opt to string")
-            .trim();
-        anyhow::bail!(
-            "The wasm-opt optimization failed.\n\n\
-            The error which wasm-opt returned was: \n{}",
-            err
-        );
-    }
-    Ok(())
-}
-
 /// Checks if the `wasm-opt` binary under `wasm_opt_path` returns a version
 /// compatible with `cargo-contract`.
 ///
@@ -958,6 +836,8 @@ const WASM_OPT_INSTALLATION_SUGGESTION: &str =
 
 struct WasmOptHandler {
     wasm_opt_path: PathBuf,
+    /// The supplied `optimization_level` denotes the number of optimization passes,
+    /// resulting in potentially a lot of time spent optimizing.
     optimization_level: OptimizationPasses,
     keep_debug_symbols: bool,
     // Take these from optimize_wasm
@@ -986,6 +866,9 @@ impl WasmOptHandler {
         })
     }
 
+    /// Attempts to perform optional Wasm optimization using `binaryen`'s `wasm-opt` tool.
+    ///
+    /// If successful, the optimized Wasm is written to `dest_optimized`.
     fn optimize(
         &self,
         dest_wasm: &PathBuf,
