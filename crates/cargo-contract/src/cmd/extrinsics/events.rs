18089d05affc719666bd89442a278a4e73001f23
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-10-06 12:49:47 +0200

    Log failure instead of failing

--- crates/cargo-contract/src/cmd/extrinsics/events.rs
+++ crates/cargo-contract/src/cmd/extrinsics/events.rs
@@ -107,9 +107,14 @@ impl DisplayEvents {
                 ) && field.name() == Some("data")
                 {
                     tracing::debug!("event data: {:?}", hex::encode(&event_data));
-                    let contract_event = transcoder.decode_contract_event(event_data)?;
-                    let field = Field::new(String::from("data"), contract_event);
-                    event_entry.fields.push(field);
+                    if let Result::Ok(contract_event) =
+                        transcoder.decode_contract_event(event_data)
+                    {
+                        let field = Field::new(String::from("data"), contract_event);
+                        event_entry.fields.push(field);
+                    } else {
+                        tracing::warn!("Decoding contract event failed. It might have come from another contract.");
+                    }
                 } else {
                     let field_name =
                         field.name().map(ToOwned::to_owned).unwrap_or_else(|| {
