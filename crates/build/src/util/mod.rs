b90369df05b91540a9aed7be159474f31bc3d2af
Andrew Jones <ascjones@gmail.com>
2023-05-17 11:41:13 +0100

    Only detect term size on non windows OS

--- crates/build/src/util/mod.rs
+++ crates/build/src/util/mod.rs
@@ -93,11 +93,14 @@ where
 
 /// Configures the cargo command to output colour and the progress bar.
 pub fn cargo_tty_output(cmd: Expression) -> Expression {
-    // let term_size = term_size::dimensions_stderr()
-    //     .map(|(width, _)| width.to_string())
-    //     .unwrap_or_else(|| "100".to_string());
-    use term_size::dimensions_stderr;
+    #[cfg(windows)]
     let term_size = "100";
+
+    #[cfg(not(windows))]
+    let term_size = term_size::dimensions_stderr()
+        .map(|(width, _)| width.to_string())
+        .unwrap_or_else(|| "100".to_string());
+
     cmd.env("CARGO_TERM_COLOR", "always")
         .env("CARGO_TERM_PROGRESS_WIDTH", term_size)
         .env("CARGO_TERM_PROGRESS_WHEN", "always")
