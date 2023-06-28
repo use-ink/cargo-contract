0a1d67576128914092bf8145e3cecadeef98d7d4
Joshy Orndorff <admin@joshyorndorff.com>
2023-06-28 12:05:46 -0400

    sketch out new config

--- crates/cargo-contract/src/cmd/mod.rs
+++ crates/cargo-contract/src/cmd/mod.rs
@@ -41,9 +41,35 @@ pub(crate) use self::extrinsics::{
 use subxt::{
     Config,
     OnlineClient,
+    // We bring this one in so that we can extend and override its types.
+    // We could consider using polkadot's instead. They differ only in the 
+    SubstrateConfig, config::extrinsic_params::BaseExtrinsicParams,
 };
 
-pub use subxt::PolkadotConfig as DefaultConfig;
 type Client = OnlineClient<DefaultConfig>;
 type Balance = u128;
 type CodeHash = <DefaultConfig as Config>::Hash;
+
+// Here is a core change. Instead of using the Polkadot config,
+// We create our own that has the ethereum-style signature and accounts
+// pub use subxt::PolkadotConfig as DefaultConfig;
+pub use AcademyPowConfig as DefaultConfig;
+
+/// A runtime configuration for the academy pow chain.
+/// This thing is not meant to be instantiated; it is just a collection of types.
+pub enum AcademyPowConfig{}
+impl subxt::Config for DefaultConfig {
+    type Index = <SubstrateConfig as Config>::Index;
+    type Hash = <SubstrateConfig as Config>::Hash;
+    type AccountId = account::AccountId20;
+    type Address = account::AccountId20;
+    type Signature = account::EthereumSignature;
+    type Hasher = <SubstrateConfig as Config>::Hasher;
+    type Header = <SubstrateConfig as Config>::Header;
+    type ExtrinsicParams = AcademyPowExtrinsicParams<Self>;
+}
+
+/// A struct representing the signed extra and additional parameters required
+/// to construct a transaction for an academy pow node. This is actually a direct copy
+/// of PolkadotExtrinsicParams, but I wanted to make it more clear that it is for the plain tip.
+pub type AcademyPowExtrinsicParams<T> = BaseExtrinsicParams<T, subxt::config::polkadot::PlainTip>;
