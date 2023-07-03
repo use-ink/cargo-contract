ed2c687800e9cc41b81b80fc9e11a5b6b6339ac1
Alexander Theißen <alex.theissen@me.com>
2023-07-03 11:56:55 +0200

    Enable Wasm sign_ext (#1189)
    * Enable Wasm sign_ext
    
    * Bump substrate crates
    
    * Add warning when building with new rustc
    
    * Stable toolchain
    
    * Revert "Stable toolchain"
    
    This reverts commit aa2a2e6f5c53b831b8bac83cc9c9a97cc700708e.
    
    ---------
    
    Co-authored-by: Andrew Jones <ascjones@gmail.com>

--- crates/build/src/lib.rs
+++ crates/build/src/lib.rs
@@ -274,6 +274,18 @@ fn exec_cargo_for_onchain_target(
             // Allow nightly features on a stable toolchain
             env.push(("RUSTC_BOOTSTRAP", Some("1".to_string())))
         }
+
+        // newer rustc versions might emit unsupported instructions
+        if rustc_version::version_meta()?.semver >= Version::new(1, 70, 0) {
+            maybe_println!(
+                verbosity,
+                "{} {}",
+                "warning:".yellow().bold(),
+                "You are using a rustc version that might emit unsupported wasm instructions. Build using a version lower than 1.70.0 to make sure your contract will deploy."
+                    .bold()
+            );
+        }
+
         // the linker needs our linker script as file
         let rustflags = target.rustflags();
         if matches!(target, Target::RiscV) {
