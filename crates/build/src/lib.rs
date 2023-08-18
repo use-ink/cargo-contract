756aa3859ee45b072df2bd09e924a6d323d47c39
Andrew Jones <ascjones@gmail.com>
2023-08-18 11:34:17 +0100

    Add `SignextLowering` pass to `wasm-opt` (#1280)
    * Add `SignextLowering` pass to `wasm-opt`
    
    * Remove check for Rust version

--- crates/build/src/lib.rs
+++ crates/build/src/lib.rs
@@ -308,16 +308,6 @@ fn exec_cargo_for_onchain_target(
             env.push(("RUSTC_BOOTSTRAP", Some("1".to_string())))
         }
 
-        // newer rustc versions might emit unsupported instructions
-        if rustc_version::version_meta()?.semver >= Version::new(1, 70, 0) {
-            eprintln!(
-                "{} {}",
-                "warning:".yellow().bold(),
-                "You are using a rustc version that might emit unsupported wasm instructions. Build using a version lower than 1.70.0 to make sure your contract will deploy."
-                    .bold()
-            );
-        }
-
         // merge target specific flags with the common flags (defined here)
         // We want to disable warnings here as they will be duplicates of the clippy pass.
         // However, if we want to do so with either `--cap-lints allow` or  `-A
