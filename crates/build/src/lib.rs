1d1560166f1ad534258c7cbd08520e52fefd4eea
Georgiy Komarov <jubnzv@gmail.com>
2023-12-23 16:11:08 -0300

    feat: Print more context on error when running `cargo`

--- crates/build/src/lib.rs
+++ crates/build/src/lib.rs
@@ -450,7 +450,7 @@ fn execute_cargo(cargo: duct::Expression) -> Result<()> {
     match cargo.unchecked().run() {
         Ok(out) if out.status.success() => Ok(()),
         Ok(out) => anyhow::bail!(String::from_utf8_lossy(&out.stderr).to_string()),
-        Err(e) => anyhow::bail!("Cannot run `cargo` command"),
+        Err(e) => anyhow::bail!("Cannot run `cargo` command: {:?}", e),
     }
 }
 
