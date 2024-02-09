9bbe8c4d003f5dc4f8e51287761837f96305eb94
Andrew Jones <ascjones@gmail.com>
2024-02-09 16:19:55 +0000

    :t-rex: fix clippy iterator suggestion

--- crates/extrinsics/src/contract_storage.rs
+++ crates/extrinsics/src/contract_storage.rs
@@ -204,7 +204,7 @@ impl Mapping {
     }
 
     /// Iterate all key-value pairs.
-    pub fn iter(&self) -> impl Iterator<Item = &(Value, Value)> + DoubleEndedIterator {
+    pub fn iter(&self) -> impl Iterator<Item = &(Value, Value)> {
         self.map.iter()
     }
 }
