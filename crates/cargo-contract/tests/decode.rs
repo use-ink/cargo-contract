14a7892850818c1bdeb816fc7e380ee1214ef0c6
Michael Mueller <mich@elmueller.net>
2025-01-23 20:22:42 +0100

    Remove `debug-ci`

--- crates/cargo-contract/tests/decode.rs
+++ crates/cargo-contract/tests/decode.rs
@@ -14,9 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with cargo-contract.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::{
-    path::Path,
-};
+use std::path::Path;
 
 /// Create a `cargo contract` command
 fn cargo_contract<P: AsRef<Path>>(path: P) -> assert_cmd::Command {
