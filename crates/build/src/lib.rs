d0c468e64494f6031e89d6d5d5dbaf07f27ac12a
Michael Mueller <mich@elmueller.net>
2025-03-26 10:39:52 +0100

    About immediately in non-debug

--- crates/build/src/lib.rs
+++ crates/build/src/lib.rs
@@ -306,8 +306,7 @@ fn exec_cargo_for_onchain_target(
         if build_mode == &BuildMode::Debug {
             features.push("ink/ink-debug");
         } else {
-            // todo
-            //args.push("-Zbuild-std-features=panic_immediate_abort".to_owned());
+            args.push("-Zbuild-std-features=panic_immediate_abort".to_owned());
         }
         features.append_to_args(&mut args);
         let mut env = Vec::new();
