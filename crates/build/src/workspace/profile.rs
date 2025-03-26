a764c17e1fd8bc6eb0c633cd31dc76d1fa4264b0
Michael Mueller <mich@elmueller.net>
2025-03-26 10:23:34 +0100

    Revert to `Lto::Fat`

--- crates/build/src/workspace/profile.rs
+++ crates/build/src/workspace/profile.rs
@@ -34,9 +34,7 @@ impl Profile {
     pub fn default_contract_release() -> Profile {
         Profile {
             opt_level: Some(OptLevel::Z),
-            // todo change back to `Lto::Fat` once bug in `polkavm` has been fixed.
-            // currently `Lto::Fat` results in the `contract-transfer` e2e tests to fail.
-            lto: Some(Lto::Off),
+            lto: Some(Lto::Fat),
             codegen_units: Some(1),
             panic: Some(PanicStrategy::Abort),
         }
